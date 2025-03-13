ce233f3d77a3e77c86dcd0256207f1e55b4477c8
Samuele Catania <samuele.catania@hotmail.com>
2025-03-13 11:45:54 +0100

    Create Input Manager

---
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private InputActionManager inputActions;
+
+    void Awake()
+    {
+        inputActions = new InputActionManager();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+#region Player Inputs
+    public Vector2 GetPlayerMovement() =>
+        inputActions.Player.Move.ReadValue<Vector2>();
+
+    // public bool GetPlayerSprint(){
+    //    return inputActions.Player.Sprint.ReadValue<bool>(); 
+    // }
+
+    public bool GetPlayerInteract() =>
+        inputActions.Player.Interact.triggered;
+    
+    public void PauseGame()
+    {
+        inputActions.Player.Disable();
+        inputActions.UI.Enable();
+    }
+#endregion
+
+#region UI Inputs
+    public bool UISelect() =>
+        inputActions.UI.Select.triggered;
+
+    public void UnpauseGame()
+    {
+        inputActions.UI.Disable();
+        inputActions.Player.Enable();
+    }
+    #endregion
+}
