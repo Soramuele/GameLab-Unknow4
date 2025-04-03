104b2ee7520d4151a25cd960c205cd83e967aaf2
Cucumbet <ruslanrudne17@gmail.com>
2025-04-03 12:28:00 +0200

    Level3SemiDone

---
+++ Assets/RR/Scripts/WindowMechanic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowMechanic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator windowanimator;
+    public GameObject player;
+  
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerOpenWindow();
+    }
+
+
+    private void PlayerOpenWindow()
+    {
+
+        if (Vector3.Distance(this.transform.position, player.transform.position) < 3 && Input.GetKeyDown(KeyCode.E))
+        {
+
+            windowanimator.SetBool("WindowOff", false);
+        }
+
+
+       
+
+
+    }
+
+   
+}
---
+++ Assets/Scripts/StudentMechanic.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StudentMechanic : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Window;
+    private NavMeshAgent Agent;
+    public WindowMechanic windowscript;
+    public GameObject startposition;
+    float seconds;
+ 
+
+    
+
+
+
+    void Start()
+    {
+
+        Agent = GetComponent<NavMeshAgent>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds += Time.deltaTime;
+
+
+
+
+        print(seconds);
+   
+
+        WindowClosing();
+
+
+
+
+
+    }
+
+    public void WindowClosing()
+    {
+
+        if(seconds > 15 && !windowscript.windowanimator.GetBool("WindowOff"))
+        
+        {
+            Agent.enabled = true;
+            Agent.SetDestination(Window.transform.position);
+          
+        
+        }
+
+
+
+        if (Vector3.Distance(transform.position, Window.transform.position) < 2.5f)
+        {
+            windowscript.windowanimator.SetBool("WindowOff", true);
+            Agent.SetDestination(startposition.transform.position);
+
+            seconds = 0;
+
+
+
+        }
+
+
+     
+    }
+
+}
